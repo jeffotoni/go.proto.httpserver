31e08efc0ce816969d9bff9fb9f3055e61f69b1b
jefferson otoni <jeff.otoni@s3wf.com.br>
2020-08-03 00:11:02 -0300

    update correction

--- main.go
+++ main.go
@@ -1,22 +1,33 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/golang/protobuf/proto"
-	protoc "github.com/jeffotoni/go.protobuffers/customer"
+	protoc "github.com/jeffotoni/go.protobuffer.customer"
 )
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/customer/proto", Customer)
+	println("Run Server: 8080")
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
 
 func Customer(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(400)
+		w.Write([]byte(`{"msg":"error method!"}`))
+		return
+	}
+	if r.Header.Get("Content-Type") != "application/proto" {
+		w.WriteHeader(400)
+		w.Write([]byte(`{"msg":"error content-type!"}`))
+		return
+	}
 	b, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		w.WriteHeader(400)
@@ -28,11 +39,9 @@ func Customer(w http.ResponseWriter, r *http.Request) {
 	err = proto.Unmarshal(b, pCustomer)
 	if err != nil {
 		w.WriteHeader(400)
-		log.Fatal("unmarshaling error: ", err)
+		log.Println("unmarshaling error invalid: ", err)
 		return
 	}
-
-	fmt.Println(pCustomer)
 	w.WriteHeader(200)
-	w.Write([]byte("ok"))
+	w.Write([]byte(`{"id":` + strconv.Itoa(int(pCustomer.Id)) + `,"name":"` + pCustomer.Name + `"}`))
 }
